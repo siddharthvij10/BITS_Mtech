c02b8a478ec849c2c88066af7799b73caed0846c
siddharthvij10 <siddharth.vij10@gmail.com>
2023-05-21 00:01:05 +0530

    Add files via upload

--- Azure/Deployment/echo_score.py
+++ Azure/Deployment/echo_score.py
@@ -4,60 +4,41 @@ import pandas as pd
 import os
 from sklearn.preprocessing import PowerTransformer
 from sklearn.preprocessing import StandardScaler
-
 from sklearn.preprocessing import OneHotEncoder
 
 
-
 def init():
-    print("This is init")
-    global loaded_model
-    loaded_model = joblib.load(os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_artifacts/random_forest_v4.joblib"))
-    global power
-    power = joblib.load(os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_artifacts/power.bin"))
-    global std_scaler
-    std_scaler=joblib.load(os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_artifacts/std_scaler.bin"))
-    global ohe
-    ohe=joblib.load(os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_artifacts/ohe.bin"))
-    global WindGustDir_FE
-    WindGustDir_FE = joblib.load(os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_artifacts/WindGustDir_FE.bin"))
-    global WindDir9am_FE
-    WindDir9am_FE = joblib.load(os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_artifacts/WindDir9am_FE.bin"))
-    global WindDir3pm_FE
-    WindDir3pm_FE = joblib.load(os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_artifacts/WindDir3pm_FE.bin"))
-    global Location_FE
-    Location_FE = joblib.load(os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_artifacts/Location_FE.bin"))
-    global le  
-    le = joblib.load(os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_artifacts/le.bin"))
-
+    global kmeans
+    kmeans = joblib.load(os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_artifacts/kmeans.joblib"))
+    global enc
+    enc = joblib.load(os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_artifacts/enc.bin"))
+    global taxi_demand_prediction
+    taxi_demand_prediction=joblib.load(os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_artifacts/taxi_demand_prediction.joblib"))
 
 
 def run(data):
     test = json.loads(data)
+    df_predict = pd.Series(test).to_frame().T
+    tpep_pickup_datetime = df_predict['tpep_pickup_datetime'].iloc[0]
+    df_predict = df_predict.drop("tpep_pickup_datetime", axis=1)
 
-    df = pd.Series(test).to_frame().T
-    skewed_features = ['WindGustSpeed', 'WindSpeed9am', 'WindSpeed3pm', 'Humidity9am']
-    df[skewed_features] = power.transform(df[skewed_features])
-
-    cols_for_standardization = ['MinTemp', 'MaxTemp', 'WindGustSpeed','WindSpeed9am', 'WindSpeed3pm','Humidity9am', 'Humidity3pm', 'Pressure9am']
-    df[cols_for_standardization] = std_scaler.transform(df[cols_for_standardization])
-
-    # Raintoday (OHE) wasn't working. So, for time being, we removed RainToday column itself from model. TO BE CHECKED
-
-    def freq_encode(df):
-        df.loc[:, 'WindGustDir_Freq_Encoded'] = df['WindGustDir'].map(WindGustDir_FE)
-        df.loc[:, 'WindDir9am_Freq_Encoded'] = df['WindDir9am'].map(WindDir9am_FE)
-        df.loc[:, 'WindDir3pm_Freq_Encoded'] = df['WindDir3pm'].map(WindDir3pm_FE)
-        df.loc[:, 'Location_Freq_Encoded'] = df['Location'].map(Location_FE)
-        df = df.drop(['WindGustDir', 'WindDir9am', 'WindDir3pm', 'Location', 'Date'], axis=1)
-        return df
-
-    df = freq_encode(df)
+    df_predict['cluster_label'] = kmeans.predict(df_predict)  
     
-    lst_cols = ['Humidity3pm', 'Pressure9am', 'Humidity9am', 'WindGustSpeed','MaxTemp', 'MinTemp', 'Location_Freq_Encoded', 'WindSpeed3pm','WindSpeed9am','WindGustDir_Freq_Encoded','WindDir3pm_Freq_Encoded',
- 'WindDir9am_Freq_Encoded']  
+    df_predict.insert(loc = 0, column = 'tpep_pickup_datetime',value = [tpep_pickup_datetime]) 
+    df_predict["tpep_pickup_datetime"] = df_predict["tpep_pickup_datetime"].apply(pd.to_datetime)
+    df_predict['pickup_hour'] = df_predict["tpep_pickup_datetime"].dt.hour
+    df_predict['pickup_dayofweek'] = df_predict["tpep_pickup_datetime"].dt.dayofweek
 
-    prediction = loaded_model.predict(df[lst_cols])   
-    prediction_category = le.inverse_transform(prediction)
+    cat_columns = ['pickup_dayofweek', 'pickup_hour', 'cluster_label']
+    datastruct_predict_encoded = enc.transform(df_predict[cat_columns]).toarray()  
+    df_predict_encoded_df = pd.DataFrame(datastruct_predict_encoded, columns=enc.get_feature_names_out(cat_columns))
+    df_predict_model = pd.concat([df_predict.reset_index(), df_predict_encoded_df], axis=1).drop(['index'], axis=1)
 
-    return f"RainTomorrow is {prediction} which encodes to {prediction_category}"
+    df_predict_model = df_predict_model.drop(cat_columns, axis=1)
+    df_predict_model = df_predict_model.drop(["pickup_latitude", "pickup_longitude" , "tpep_pickup_datetime"], axis=1)
+    
+    df_predict_model[df_predict_model.columns] = df_predict_model[df_predict_model.columns].astype(int)
+
+    prediction = taxi_demand_prediction.predict(df_predict_model)
+    prediction = round(prediction[0]*100, 2)
+    return "demand percent is {}".format(prediction)
